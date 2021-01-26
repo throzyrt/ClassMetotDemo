3ac1340054c5ea57886d83b4f83f674e8cfd5db5
throzyrt <tahirozyurt78@gmail.com>
2021-01-26 15:32:21 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi!");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Kaydı Silindi!");
+        }
+        public void Lıstele(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Id : " + musteri.Id);
+            Console.WriteLine("Musteri Ad : " + musteri.Ad);
+            Console.WriteLine("Musteri Soyad : " + musteri.Soyad);
+            Console.WriteLine("Musteri Sehir : " + musteri.Sehir);
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri
+            {
+                Id = 1,
+                Ad = "Tahir",
+                Soyad = "Özyurt",
+                Sehir = "İstanbul"
+            };
+            MusteriManager musteriManager = new MusteriManager();
+            
+            musteriManager.Lıstele(musteri);
+            musteriManager.Ekle(musteri);
+            musteriManager.Sil(musteri);
+            
+            Console.ReadLine();
+        }
+    }
+
+}
